d280ad04d4e3e822db384449dad42ba0f18c70c4
Siluat <siluat@gmail.com>
2020-10-30 01:19:44 +0900

    [딥러닝과 바둑] 3장 첫 번째 바둑봇 만들기

--- MachineLearning/딥러닝과 바둑/bot_v_bot.py
+++ MachineLearning/딥러닝과 바둑/bot_v_bot.py
@@ -1,5 +1,5 @@
 from dlgo import agent
-from dlgo import goboard_slow
+from dlgo import goboard
 from dlgo import gotypes
 from dlgo.utils import print_board, print_move
 import time
@@ -7,7 +7,7 @@ import time
 
 def main():
     board_size = 9
-    game = goboard_slow.GameState.new_game(board_size)
+    game = goboard.GameState.new_game(board_size)
     bots = {
         gotypes.Player.black: agent.naive.RandomBot(),
         gotypes.Player.white: agent.naive.RandomBot(),
