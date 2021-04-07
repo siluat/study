ba3c7a28ada5166c5bb8cfc620e8ab87da674f22
Siluat <siluat@gmail.com>
2021-04-07 23:12:57 +0900

    [이규원의 현실 세상의 TDD] 단위 테스트

---
+++ TDD/이규원의 현실 세상의 TDD/README.md
@@ -0,0 +1,3 @@
+# 이규원의 현실 세상의 TDD 학습 기록
+
+https://fastcampus.co.kr/dev_red_ygw
---
+++ TDD/이규원의 현실 세상의 TDD/code/text-refiner/index.test.js
@@ -0,0 +1,12 @@
+const { test, expect } = require("@jest/globals");
+const sut = require("./index");
+
+test.each`
+  source              | expected
+  ${"hello  world"}   | ${"hello world"}
+  ${"hello   world"}  | ${"hello world"}
+  ${"hello    world"} | ${"hello world"}
+`('sut transforms "$source" to "$expected"', ({ source, expected }) => {
+  const actual = sut(source);
+  expect(actual).toBe(expected);
+});
