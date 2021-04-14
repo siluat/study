bf977d9d591a46be13f5d38cb6fca06a76a52d34
siluat <siluat@gmail.com>
2021-04-14 23:01:54 +0900

    [이규원의 현실 세상의 TDD] 개방-폐쇄 원칙 구현 실습 종료 코드

--- TDD/이규원의 현실 세상의 TDD/code/comment-list/src/index.js
+++ TDD/이규원의 현실 세상의 TDD/code/comment-list/src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import compositeContentRefinerFactory from "./content-refiners/compositeContentRefinerFactory";
+import compactWhitespaces from "./content-refiners/compactWhitespaces";
+import trimWhitepaces from "./content-refiners/trimWhitespaces";
 import commentComposerFactory from "./commentComposerFactory";
+
+const commentRefiner = compositeContentRefinerFactory([
+  compactWhitespaces,
+  trimWhitepaces,
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App commentComposer={commentComposerFactory()} />
+    <App commentComposer={commentComposerFactory(commentRefiner)} />
   </React.StrictMode>,
   document.getElementById("root")
 );
