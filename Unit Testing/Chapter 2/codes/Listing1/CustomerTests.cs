14a4cdb3b90253dbce11aef8708e23c5c161d722
siluat <siluat@gmail.com>
2021-12-07 02:28:32 +0900

    Unit Tesing 1, 2장 공부

---
+++ Unit Testing/Chapter 2/codes/Listing1/CustomerTests.cs
@@ -0,0 +1,39 @@
+using Xunit;
+
+namespace Listing1
+{
+
+    public class CustomerTests
+    {
+        [Fact]
+        public void Purchase_succeeds_when_enough_inventory()
+        {
+            // Arrange
+            var store = new Store();
+            store.AddInventory(Product.Shampoo, 10);
+            var customer = new Customer();
+
+            // Act
+            bool success = customer.Purchase(store, Product.Shampoo, 5);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Purchase_fails_when_not_enough_inventory()
+        {
+            // Arrange
+            var store = new Store();
+            store.AddInventory(Product.Shampoo, 10);
+            var customer = new Customer();
+
+            // Act
+            bool success = customer.Purchase(store, Product.Shampoo, 15);
+
+            // Assert
+            Assert.False(success);
+            Assert.Equal(10, store.GetInventory(Product.Shampoo));
+        }
+    }
+}
