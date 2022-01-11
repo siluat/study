974b2796b3d2ae4c8220a7be00d03791e4d54924
siluat <siluat@gmail.com>
2022-01-11 21:41:05 +0900

    [Remix Blog Tutorial] a little refactoring

--- Remix/turorials/developer-blog/app/routes/posts/index.tsx
+++ Remix/turorials/developer-blog/app/routes/posts/index.tsx
@@ -1,22 +1,8 @@
 import { Link, useLoaderData } from "remix";
-
-export type Post = {
-  slug: string;
-  title: string;
-};
+import { getPosts, Post } from "~/post";
 
 export const loader = () => {
-  const posts: Post[] = [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
-  return posts;
+  return getPosts();
 };
 
 export default function Posts() {
