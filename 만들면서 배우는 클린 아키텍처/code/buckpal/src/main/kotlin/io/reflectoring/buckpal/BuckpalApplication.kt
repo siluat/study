99c0be2f6f25636e7dca2b8e860df07340e3a02d
siluat <siluat@gmail.com>
2024-12-05 19:33:41 +0900

    [만들면서 배우는 클린 아키텍처] 예제 프로젝트 초기화

---
+++ 만들면서 배우는 클린 아키텍처/code/buckpal/src/main/kotlin/io/reflectoring/buckpal/BuckpalApplication.kt
@@ -0,0 +1,11 @@
+package io.reflectoring.buckpal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuckpalApplication
+
+fun main(args: Array<String>) {
+	runApplication<BuckpalApplication>(*args)
+}
