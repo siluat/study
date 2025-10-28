afff533b6c447e8c30cad3335020cd8840fc813b
siluat <siluat@gmail.com>
2025-10-28 19:31:56 +0900

    [밑바닥부터 시작하는 웹 브라우저] 서버가 설정한 쿠키값을 호스트별로 저장하고 재요청시 쿠키값을 전송하는 기능 구현

---
+++ 밑바닥부터 시작하는 웹 브라우저/browser/src/lab10.py
@@ -0,0 +1,62 @@
+import socket
+import ssl
+import tkinter
+import wbetools
+from lab8 import URL,Browser
+
+@wbetools.patch(URL)
+class URL:
+    def request(self, payload=None):
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        s.connect((self.host, self.port))
+
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+
+        method = "POST" if payload else "GET"
+        request = "{} {} HTTP/1.0\r\n".format(method, self.path)
+        request += "Host: {}\r\n".format(self.host)
+        if self.host in COOKIE_JAR:
+            cookie = COOKIE_JAR[self.host]
+            request += "Cookie: {}\r\n".format(cookie)
+        if payload:
+            length = len(payload.encode("utf8"))
+            request += "Content-Length: {}\r\n".format(length)
+        request += "\r\n"
+        if payload: request += payload
+        s.send(request.encode("utf8"))
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n": break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+
+        if "set-cookie" in response_headers:
+            cookie = response_headers["set-cookie"]
+            COOKIE_JAR[self.host] = cookie
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        body = response.read()
+        s.close()
+
+        return body
+
+COOKIE_JAR = {}
+
+if __name__ == "__main__":
+    import sys
+    Browser().new_tab(URL(sys.argv[1]))
+    tkinter.mainloop()
