9d562cf3051a72061f651ca03561df5eceaf6041
Myunggil Kang <siluat@gmail.com>
2025-12-29 20:18:05 +0900

    [밑바닥부터 시작하는 웹 브라우저] Skia 서피스를 SDL 서피스로 복사하여 그리기

--- 밑바닥부터 시작하는 웹 브라우저/browser/src/lab11.py
+++ 밑바닥부터 시작하는 웹 브라우저/browser/src/lab11.py
@@ -1,6 +1,7 @@
 import sys
 import ctypes
 import sdl2
+import skia
 import wbetools
 from lab2 import WIDTH, HEIGHT
 from lab8 import Browser
@@ -12,15 +13,48 @@ class Browser:
         self.sdl_window = sdl2.SDL_CreateWindow(b"Browser",
             sdl2.SDL_WINDOWPOS_CENTERED, sdl2.SDL_WINDOWPOS_CENTERED,
             WIDTH, HEIGHT, sdl2.SDL_WINDOW_SHOWN)
+        self.root_surface = skia.Surface.MakeRaster(
+            skia.ImageInfo.Make(
+                WIDTH, HEIGHT,
+                ct=skia.kRGBA_8888_ColorType,
+                at=skia.kUnpremul_AlphaType))
+
+        if sdl2.SDL_BYTEORDER == sdl2.SDL_BIG_ENDIAN:
+            self.RED_MASK = 0xff000000
+            self.GREEN_MASK = 0x00ff0000
+            self.BLUE_MASK = 0x0000ff00
+            self.ALPHA_MASK = 0x000000ff
+        else:
+            self.RED_MASK = 0x000000ff
+            self.GREEN_MASK = 0x0000ff00
+            self.BLUE_MASK = 0x00ff0000
+            self.ALPHA_MASK = 0xff000000
 
     def new_tab(self, url):
         pass
 
     def draw(self):
-        pass
+        canvas = self.root_surface.getCanvas()
+        canvas.clear(skia.ColorWHITE)
+
+        skia_image = self.root_surface.makeImageSnapshot()
+        skia_bytes = skia_image.tobytes()
+
+        depth = 32 # Bits per pixel
+        pitch = 4 * WIDTH # Bytes per row
+        sdl_surface = sdl2.SDL_CreateRGBSurfaceFrom(
+            skia_bytes, WIDTH, HEIGHT, depth, pitch,
+            self.RED_MASK, self.GREEN_MASK,
+            self.BLUE_MASK, self.ALPHA_MASK)
+
+        rect = sdl2.SDL_Rect(0, 0, WIDTH, HEIGHT)
+        window_surface = sdl2.SDL_GetWindowSurface(self.sdl_window)
+        # SDL_BlitSurface is what actually does the copy.
+        sdl2.SDL_BlitSurface(sdl_surface, rect, window_surface, rect)
+        sdl2.SDL_UpdateWindowSurface(self.sdl_window)
 
     def handle_quit(self):
-        sdl2.SDL_DestoryWindow(self.sdl_window)
+        sdl2.SDL_DestroyWindow(self.sdl_window)
 
 def mainloop(browser):
     event = sdl2.SDL_Event()
